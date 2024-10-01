f19fc865957a279586a3e2d0395d35205f6a3080
dave <yefriddavid.apps@gmail.com>
2024-10-01 18:44:03 -0500

    config added

---
+++ config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"fmt"
+    "context"
+    "path/filepath"
+
+    firebase "firebase.google.com/go"
+    "firebase.google.com/go/messaging"
+    "google.golang.org/api/option"
+)
+
+func SetupFirebase() (*firebase.App, context.Context, *messaging.Client) {
+
+    ctx := context.Background()
+
+    //serviceAccountKeyFilePath, err := filepath.Abs("./serviceAccountKey.json")
+    serviceAccountKeyFilePath, err := filepath.Abs("./google-services.json")
+    if err != nil {
+        panic("Unable to load serviceAccountKeys.json file")
+    }
+
+    opt := option.WithCredentialsFile(serviceAccountKeyFilePath)
+
+    //Firebase admin SDK initialization
+    app, err := firebase.NewApp(context.Background(), nil, opt)
+    if err != nil {
+        panic("Firebase load error")
+    }
+
+    //Messaging client
+    client, _ := app.Messaging(ctx)
+
+    return app, ctx, client
+}
